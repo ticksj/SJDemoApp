2180093aa89c62000015c0979f0add7073062f3e
ticksj <1085798032@qq.com>
2019-12-30 23:07:46 +0800

    属性监听插值器估值器监听

--- b_4_animation/src/main/java/com/sj/b_4_animation/MainActivity.java
+++ b_4_animation/src/main/java/com/sj/b_4_animation/MainActivity.java
@@ -1,13 +1,21 @@
 package com.sj.b_4_animation;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 import android.view.animation.LayoutAnimationController;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
@@ -100,6 +108,30 @@ public class MainActivity extends AppCompatActivity {
                 changeObjectColor();
             }
         });
+        findViewById(R.id.object_a6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeObjectSet();
+            }
+        });
+        findViewById(R.id.object_a7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeObjectYWithInterpolatorEvaluator();
+            }
+        });
+        findViewById(R.id.object_a8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeObjectYWithListener();
+            }
+        });
+        findViewById(R.id.object_a9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeObjectYWithUpdateListener();
+            }
+        });
         findViewById(R.id.f_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,6 +139,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
     // ---------------------基础动画----------------------------------------------
     private void animationDrawable() {
         fAnimaitonView.setBackgroundResource(R.drawable.drawable_animation);
@@ -207,31 +241,162 @@ public class MainActivity extends AppCompatActivity {
         Animation a = AnimationUtils.loadAnimation(this, R.anim.base_animation);
         baseAnimationView.startAnimation(a);
     }
+
     // ---------------------属性动画----------------------------------------------
-private void changeObjectTranslationX() {
-    ObjectAnimator.ofFloat(objectView,"translationX",100)
-            .setDuration(2000)
-            .start();
-}
-private void changeObjectScaleX() {
-    ObjectAnimator.ofFloat(objectView,"scaleX",0,1,2,1)
-            .setDuration(2000)
-            .start();
-}
-private void changeObjectRotation() {
-    ObjectAnimator.ofFloat(objectView,"rotation",0,100,180,360)
-            .setDuration(2000)
-            .start();
-}
-private void changeObjectAlpha() {
-    ObjectAnimator.ofFloat(objectView,"alpha",0f,0.2f,0.5f,0.7f,1f)
-            .setDuration(2000)
-            .start();
-}
-private void changeObjectColor() {
-    ObjectAnimator.ofInt(objectView,"backgroundColor",0xFFFF0000,0xFFFB742E)
-            .setDuration(2000)
-            .start();
-}
+    private void changeObjectTranslationX() {
+        ObjectAnimator.ofFloat(objectView, "translationX", 100)
+                .setDuration(2000)
+                .start();
+    }
+
+    private void changeObjectScaleX() {
+        ObjectAnimator.ofFloat(objectView, "scaleX", 0, 1, 2, 1)
+                .setDuration(2000)
+                .start();
+    }
+
+    private void changeObjectRotation() {
+        ObjectAnimator.ofFloat(objectView, "rotation", 0, 100, 180, 360)
+                .setDuration(2000)
+                .start();
+    }
+
+    private void changeObjectAlpha() {
+        ObjectAnimator.ofFloat(objectView, "alpha", 0f, 0.2f, 0.5f, 0.7f, 1f)
+                .setDuration(2000)
+                .start();
+    }
+
+    private void changeObjectColor() {
+        ObjectAnimator.ofInt(objectView, "backgroundColor", 0xFFFF0000, 0xFFFB742E)
+                .setDuration(2000)
+                .start();
+    }
+
+    private void changeObjectSet() {
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(
+                ObjectAnimator.ofFloat(objectView, "translationX", 100),
+                ObjectAnimator.ofFloat(objectView, "scaleX", 0, 1, 2, 1),
+                ObjectAnimator.ofFloat(objectView, "rotation", 0, 100, 180, 360),
+                ObjectAnimator.ofFloat(objectView, "alpha", 0f, 0.2f, 0.5f, 0.7f, 1f),
+                ObjectAnimator.ofInt(objectView, "backgroundColor", 0xFFFF0000, 0xFFFB742E)
+        );
+//        set.setDuration(2000).start();
+        set = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.obejct_animation_set);
+        set.setTarget(objectView);
+        set.start();
+    }
+
+
+    private void changeObjectYWithInterpolatorEvaluator() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(objectView, "translationY", 0, 1000);
+        animator.setDuration(2000);
+        animator.setInterpolator(new Interpolator() {
+            @Override
+            public float getInterpolation(float input) {
+                Log.e(TAG, "getInterpolation: =" + input);
+                return input;
+            }
+        });
+        animator.setEvaluator(new TypeEvaluator<Float>() {
+            @Override
+            public Float evaluate(float fraction, Float startValue, Float endValue) {
+                float v = startValue + fraction * (endValue - startValue);
+                Log.e(TAG, "evaluate: fraction = " + fraction + "currV==" + v);
+                return v;
+            }
+        });
+        animator.start();
+    }
+
+    private void changeObjectYWithListener() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(objectView, "translationY", 0, 1000);
+        animator.setDuration(2000);
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                Log.e(TAG, "onAnimationStart: ");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                Log.e(TAG, "onAnimationEnd: " );
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                super.onAnimationCancel(animation);
+                Log.e(TAG, "onAnimationCancel: " );
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                super.onAnimationRepeat(animation);
+                Log.e(TAG, "onAnimationRepeat: ");
+            }
+            @Override
+            public void onAnimationPause(Animator animation) {
+                super.onAnimationPause(animation);
+                Log.e(TAG, "onAnimationPause: " );
+            }
+
+            @Override
+            public void onAnimationResume(Animator animation) {
+                super.onAnimationResume(animation);
+                Log.e(TAG, "onAnimationResume: " );
+            }
+        });
+        animator.addPauseListener(new Animator.AnimatorPauseListener() {
+            @Override
+            public void onAnimationPause(Animator animation) {
+                //暂停时调用
+            }
+
+            @Override
+            public void onAnimationResume(Animator animation) {
+                //恢复时调用
+            }
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                //开始时调用
+                Log.e(TAG, "onAnimationStart: ");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //结束时调用
+                Log.e(TAG, "onAnimationEnd: " );
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                //取消时调用
+                Log.e(TAG, "onAnimationCancel: " );
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                //重复时调用
+                Log.e(TAG, "onAnimationRepeat: ");
+            }
+        });
+        animator.start();
+    }
+
+    private void changeObjectYWithUpdateListener() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(objectView, "translationY", 0, 1000);
+        animator.setDuration(2000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Log.e(TAG, "onAnimationUpdate: animation.getAnimatedFraction()=="+animation.getAnimatedFraction() );
+            }
+        });
+    }
 
 }
