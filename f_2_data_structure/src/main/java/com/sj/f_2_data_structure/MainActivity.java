b22c4a91c6b996ed67041c01bb09c3d9e943bbea
ticksj <1085798032@qq.com>
2020-02-03 10:05:50 +0800

    数据结构

---
+++ f_2_data_structure/src/main/java/com/sj/f_2_data_structure/MainActivity.java
@@ -0,0 +1,24 @@
+package com.sj.f_2_data_structure;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        XianXingList();
+    }
+
+    private void XianXingList() {
+        //源码解析
+
+        ArrayList<String> list = new ArrayList<>();
+
+    }
+}
